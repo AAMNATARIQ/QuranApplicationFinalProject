4b902318e8e125da1eb7027921869b25efc1ec9a
Amna <aamnaht4@gmail.com>
2023-02-12 22:26:10 +0500

    addimg data to files

--- app/src/main/java/amnatariq/org/quranapplicationfinalproject/d3.java
+++ app/src/main/java/amnatariq/org/quranapplicationfinalproject/d3.java
@@ -1,14 +1,32 @@
 package amnatariq.org.quranapplicationfinalproject;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class d3 extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d3);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationview);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar);
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
