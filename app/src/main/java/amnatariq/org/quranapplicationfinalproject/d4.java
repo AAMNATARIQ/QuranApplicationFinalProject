85b0b832ff51017a6f1b4b17245ebb99c2f415ac
Amna <aamnaht4@gmail.com>
2023-02-13 10:44:04 +0500

    added code to inflate view in adapter file

--- app/src/main/java/amnatariq/org/quranapplicationfinalproject/d4.java
+++ app/src/main/java/amnatariq/org/quranapplicationfinalproject/d4.java
@@ -1,14 +1,24 @@
 package amnatariq.org.quranapplicationfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class d4 extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d4);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new Adapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
