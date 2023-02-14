117437bf08816eb1f029709108f44aeb2987db0d
Amna <aamnaht4@gmail.com>
2023-02-14 18:06:03 +0500

    added getter and setter functions with respect to the member variables of the class

--- app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
+++ app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
@@ -59,4 +59,220 @@ public class QuranModel
         this.PushtuTranslation = pushtuTranslation;
         this.PushtoTafseer = pushtoTafseer;
     }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getNumberInSurah() {
+        return numberInSurah;
+    }
+
+    public void setNumberInSurah(int numberInSurah) {
+        this.numberInSurah = numberInSurah;
+    }
+
+    public int getJuz() {
+        return juz;
+    }
+
+    public void setJuz(int juz) {
+        this.juz = juz;
+    }
+
+    public int getManzil() {
+        return manzil;
+    }
+
+    public void setManzil(int manzil) {
+        this.manzil = manzil;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getRuku() {
+        return ruku;
+    }
+
+    public void setRuku(int ruku) {
+        this.ruku = ruku;
+    }
+
+    public int getHizbQuarter() {
+        return hizbQuarter;
+    }
+
+    public void setHizbQuarter(int hizbQuarter) {
+        this.hizbQuarter = hizbQuarter;
+    }
+
+    public int getSajda() {
+        return sajda;
+    }
+
+    public void setSajda(int sajda) {
+        this.sajda = sajda;
+    }
+
+    public int getSurah_number() {
+        return surah_number;
+    }
+
+    public void setSurah_number(int surah_number) {
+        this.surah_number = surah_number;
+    }
+
+    public String getSurah_name() {
+        return surah_name;
+    }
+
+    public void setSurah_name(String surah_name) {
+        this.surah_name = surah_name;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getEnglishNameTransaltion() {
+        return englishNameTransaltion;
+    }
+
+    public void setEnglishNameTransaltion(String englishNameTransaltion) {
+        this.englishNameTransaltion = englishNameTransaltion;
+    }
+
+    public String getRevelationType() {
+        return revelationType;
+    }
+
+    public void setRevelationType(String revelationType) {
+        this.revelationType = revelationType;
+    }
+
+    public int getSajda_id() {
+        return sajda_id;
+    }
+
+    public void setSajda_id(int sajda_id) {
+        this.sajda_id = sajda_id;
+    }
+
+    public int getSajda_recommended() {
+        return sajda_recommended;
+    }
+
+    public void setSajda_recommended(int sajda_recommended) {
+        this.sajda_recommended = sajda_recommended;
+    }
+
+    public int getSajda_obligatory() {
+        return sajda_obligatory;
+    }
+
+    public void setSajda_obligatory(int sajda_obligatory) {
+        this.sajda_obligatory = sajda_obligatory;
+    }
+
+    public String getUrduTransaltion() {
+        return UrduTransaltion;
+    }
+
+    public void setUrduTransaltion(String urduTransaltion) {
+        UrduTransaltion = urduTransaltion;
+    }
+
+    public String getUrduTafseer() {
+        return UrduTafseer;
+    }
+
+    public void setUrduTafseer(String urduTafseer) {
+        UrduTafseer = urduTafseer;
+    }
+
+    public String getEnglishTranslation() {
+        return EnglishTranslation;
+    }
+
+    public void setEnglishTranslation(String englishTranslation) {
+        EnglishTranslation = englishTranslation;
+    }
+
+    public String getEnglishTafseer() {
+        return EnglishTafseer;
+    }
+
+    public void setEnglishTafseer(String englishTafseer) {
+        EnglishTafseer = englishTafseer;
+    }
+
+    public String getSindhiTranslation() {
+        return SindhiTranslation;
+    }
+
+    public void setSindhiTranslation(String sindhiTranslation) {
+        SindhiTranslation = sindhiTranslation;
+    }
+
+    public String getSindhiTafseer() {
+        return SindhiTafseer;
+    }
+
+    public void setSindhiTafseer(String sindhiTafseer) {
+        SindhiTafseer = sindhiTafseer;
+    }
+
+    public String getHindiTranslation() {
+        return HindiTranslation;
+    }
+
+    public void setHindiTranslation(String hindiTranslation) {
+        HindiTranslation = hindiTranslation;
+    }
+
+    public String getHindiTafseer() {
+        return HindiTafseer;
+    }
+
+    public void setHindiTafseer(String hindiTafseer) {
+        HindiTafseer = hindiTafseer;
+    }
+
+    public String getPushtuTranslation() {
+        return PushtuTranslation;
+    }
+
+    public void setPushtuTranslation(String pushtuTranslation) {
+        PushtuTranslation = pushtuTranslation;
+    }
+
+    public String getPushtoTafseer() {
+        return PushtoTafseer;
+    }
+
+    public void setPushtoTafseer(String pushtoTafseer) {
+        PushtoTafseer = pushtoTafseer;
+    }
 }
