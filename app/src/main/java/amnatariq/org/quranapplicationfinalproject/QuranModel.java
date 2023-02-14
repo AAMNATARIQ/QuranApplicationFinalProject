df27ec86449bb25fdaa6e87d8caf4c3f95f5e3f9
Amna <aamnaht4@gmail.com>
2023-02-14 18:05:10 +0500

    adding constructor to quran model class

--- app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
+++ app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
@@ -30,4 +30,33 @@ public class QuranModel
     public String PushtuTranslation;
     public String PushtoTafseer;
 
+    public QuranModel(int number, String text, int numberInSurah, int juz, int manzil, int page, int ruku, int hizbQuarter, int sajda, int surah_number, String surah_name, String englishName, String englishNameTransaltion, String revelationType, int sajda_id, int sajda_recommended, int sajda_obligatory, String urduTransaltion, String urduTafseer, String englishTranslation, String englishTafseer, String sindhiTranslation, String sindhiTafseer, String hindiTranslation, String hindiTafseer, String pushtuTranslation, String pushtoTafseer) {
+        this.number = number;
+        this.text = text;
+        this.numberInSurah = numberInSurah;
+        this.juz = juz;
+        this.manzil = manzil;
+        this.page = page;
+        this.ruku = ruku;
+        this.hizbQuarter = hizbQuarter;
+        this.sajda = sajda;
+        this.surah_number = surah_number;
+        this.surah_name = surah_name;
+        this.englishName = englishName;
+        this.englishNameTransaltion = englishNameTransaltion;
+        this.revelationType = revelationType;
+        this.sajda_id = sajda_id;
+        this.sajda_recommended = sajda_recommended;
+        this.sajda_obligatory = sajda_obligatory;
+        this.UrduTransaltion = urduTransaltion;
+        this.UrduTafseer = urduTafseer;
+        this.EnglishTranslation = englishTranslation;
+        this.EnglishTafseer = englishTafseer;
+        this.SindhiTranslation = sindhiTranslation;
+        this.SindhiTafseer = sindhiTafseer;
+        this.HindiTranslation = hindiTranslation;
+        this.HindiTafseer = hindiTafseer;
+        this.PushtuTranslation = pushtuTranslation;
+        this.PushtoTafseer = pushtoTafseer;
+    }
 }
