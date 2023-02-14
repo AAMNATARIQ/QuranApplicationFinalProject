0a6797e8b3c9372113296def06d29d411f2b17c6
Amna <aamnaht4@gmail.com>
2023-02-14 18:01:50 +0500

    added quranmodel class and added member variables to it

--- app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
+++ app/src/main/java/amnatariq/org/quranapplicationfinalproject/QuranModel.java
@@ -2,4 +2,32 @@ package amnatariq.org.quranapplicationfinalproject;
 
 public class QuranModel
 {
+    public int number;
+    public String text;
+    public int numberInSurah;
+    public int juz;
+    public int manzil;
+    public int page;
+    public int ruku;
+    public int hizbQuarter;
+    public int sajda;
+    public int surah_number;
+    public String surah_name;
+    public String englishName;
+    public String englishNameTransaltion;
+    public String revelationType;
+    public int sajda_id;
+    public int sajda_recommended;
+    public int sajda_obligatory;
+    public String UrduTransaltion;
+    public String UrduTafseer;
+    public String EnglishTranslation;
+    public String EnglishTafseer;
+    public String SindhiTranslation;
+    public String SindhiTafseer;
+    public String HindiTranslation;
+    public String HindiTafseer;
+    public String PushtuTranslation;
+    public String PushtoTafseer;
+
 }
